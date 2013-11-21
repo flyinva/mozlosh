0af6703a0ee79f2d32008aea21518f049ff4849d
Flyinvap <flyinvap@kabano.net>
2013-11-22 00:38:38 +0100

    Interface removed from iwlist
    iwlist will scan all network interfaces
    using 2>&1 to suppress error messages for wired and lo interfaces

--- mozlo.sh
+++ mozlo.sh
@@ -4,7 +4,7 @@ IW='sudo /sbin/iwlist'
 API='https://location.services.mozilla.com/v1/search'
 
 echo $(echo -n '{ "wifi": [' ;\
-$IW $1 scan |\
+$IW scanning 2>&1 |\
 grep -e Address: -e level= -e Frequency -e Channel: |\
 sed \
 -e 's/.*Cell .* Address: \(.*\)/{"key":"\1",/' -e 's/.*Channel:\(.*\)/"channel":\1,/' -e 's/.*Frequency:\([0-9]\)\.\([0-9]*\)/"frequency":\1\2,/' -e 's/ GHz (.*)//' -e 's/.*level=//' -e 's/\(-[0-9]*\) dBm/"signal":"\1"},/' |\
